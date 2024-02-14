b8d2a0981bb0e57f96aaab7dc38c7ac8eb2f47b0
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-14 11:24:21 +0545

    home page for personal details

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-// import Banner from "./components/banner/Banner";
+import Banner from "./components/banner/Banner";
 // import Contact from "./components/contact/Contact";
 // import Features from "./components/features/Features";
 // import Footer from "./components/footer/Footer";
 // import FooterBottom from "./components/footer/FooterBottom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar/Navbar";
 // import Projects from "./components/projects/Projects";
 // import Resume from "./components/resume/Resume";
 // import Testimonial from "./components/tesimonial/Testimonial";
@@ -14,7 +14,7 @@ function App() {
     <div className="w-full h-auto bg-bodyColor text-lightText px-4">
         <Navbar />
       <div className="max-w-screen-xl mx-auto">
-        {/* <Banner /> */}
+        <Banner />
         {/* <Features />
         <Projects />
         <Resume />
---
+++ portfolio/src/components/banner/LeftBanner.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import Media from './Media';
+
+const LeftBanner = () => {
+    const [text] = useTypewriter({
+      words: ["Professional Coder.", "Full Stack Developer.", "FIGMA Designer."],
+      loop: true,
+      typeSpeed: 20,
+      deleteSpeed: 10,
+      delaySpeed: 2000,
+    });
+  return (
+    <div className="w-full lgl:w-1/2 flex flex-col gap-20">
+      <div className="flex flex-col gap-5">
+        <h4 className=" text-lg font-normal">WELCOME TO MY WORLD</h4>
+        <h1 className="text-6xl font-bold text-white">
+          Hi, I'm <span className="text-designColor capitalize">KISMAT BOHORA</span>
+        </h1>
+        <h2 className="text-4xl font-bold text-white">
+          a <span>{text}</span>
+          <Cursor
+            cursorBlinking="false"
+            cursorStyle="|"
+            cursorColor="#ff014f"
+          />
+        </h2>
+        <p className="text-base font-bodyFont leading-6 tracking-wide">
+          I use animation as a third dimension by which to simplify experiences
+          and kuiding thro each and every interaction. I'm not adding motion
+          just to spruce things up, but doing it in ways that.
+        </p>
+      </div>
+     <Media />
+    </div>
+  );
+}
+
+export default LeftBanner
