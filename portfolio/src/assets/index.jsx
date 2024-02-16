f49b28381f0098e6e5e855dbeb2ca568ecf52369
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-16 12:55:04 +0545

    image

--- portfolio/src/assets/index.jsx
+++ portfolio/src/assets/index.jsx
@@ -1,21 +1,21 @@
-import logo from "./images/logo.png";
-import bannerImg from "./images/bannerImg.png";
-import projectOne from "./images/projects/projectOne.jpg";
-import projectTwo from "./images/projects/projectTwo.jpg";
-import projectThree from "./images/projects/projectThree.jpeg";
-import testimonialOne from "./images/testmonial/testimonialOne.png";
-import testimonialTwo from "./images/testmonial/testimonialTwo.png";
-import quote from "./images/testmonial/quote.png";
-import contactImg from "./images/contact/contactImg.png";
+// import logo from "./images/logo.png";
+// import bannerImg from "./images/bannerImg.png";
+import projectOne from "./projects/projectOne.jpg";
+import projectTwo from "./projects/projectTwo.jpg";
+// import projectThree from "./images/projects/projectThree.jpeg";
+// import testimonialOne from "./images/testmonial/testimonialOne.png";
+// import testimonialTwo from "./images/testmonial/testimonialTwo.png";
+// import quote from "./images/testmonial/quote.png";
+// import contactImg from "./images/contact/contactImg.png";
 
 export {
-  logo,
-  bannerImg,
+//   logo,
+//   bannerImg,
   projectOne,
-  projectTwo,
-  projectThree,
-  testimonialOne,
-  testimonialTwo,
-  quote,
-  contactImg,
+  projectTwo
+//   projectThree,
+//   testimonialOne,
+//   testimonialTwo,
+//   quote,
+//   contactImg,
 };
