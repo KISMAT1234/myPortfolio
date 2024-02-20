7c035066cd91954bcf1c2dce207fd706b6cfd5f1
KISMAT1234 <kismatbohora123@gmail.com>
2024-02-20 14:49:53 +0545

    frontend and backend loop added

--- portfolio/src/components/banner/LeftBanner.jsx
+++ portfolio/src/components/banner/LeftBanner.jsx
@@ -4,7 +4,7 @@ import Media from './Media';
 
 const LeftBanner = () => {
     const [text] = useTypewriter({
-      words: ["Professional Coder.", "Full Stack Developer.", "FIGMA Designer."],
+      words: ["Professional Coder.","FIGMA Designer.","Frontend Developer.","Backend Developer", "Full Stack Developer.", ],
       loop: true,
       typeSpeed: 20,
       deleteSpeed: 10,
