0420046d8c2cd613b2fcdc44a6b27378ccf8fed3
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-02 21:58:17 +0545

    left banner edited

--- portfolio/src/components/banner/LeftBanner.jsx
+++ portfolio/src/components/banner/LeftBanner.jsx
@@ -25,9 +25,7 @@ const LeftBanner = () => {
           />
         </h2>
         <p className="text-base font-bodyFont leading-6 tracking-wide">
-          I use animation as a third dimension by which to simplify experiences
-          and kuiding thro each and every interaction. I'm not adding motion
-          just to spruce things up, but doing it in ways that.
+            I'm a passionate and dedicated full-stack web developer with experience in building responsive websites using React, Nodejs ,Express js and Mongo DB.
         </p>
       </div>
     </div>
