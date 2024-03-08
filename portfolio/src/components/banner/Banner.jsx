b1e8f9fe59aa0c7788018aa0cfc249ebe56348c3
KISMAT1234 <kismatbohora123@gmail.com>
2024-03-08 13:37:33 +0545

    bann

--- portfolio/src/components/banner/Banner.jsx
+++ portfolio/src/components/banner/Banner.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import LeftBanner from './LeftBanner';
 import RightBanner from './RightBanner';
+
 const Banner = () => {
   return (
     <section
       id="home"
       className="w-full pt-10 pb-20 flex flex-col gap-10 xl:gap-0 lgl:flex-row items-center border-b-[1px] font-titleFont border-b-black"
     >
+      
       <LeftBanner />
      <RightBanner />
     </section>
